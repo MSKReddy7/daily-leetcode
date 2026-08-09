be2cc1c467344e66822b943dd1c109ef8b92be4d
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-09 22:22:27 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.58%) - LeetHub

--- LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int x = n;
-        while(x &&  (x*(x+1)/2 != n*(n+1)/2 - (x-1)*(x)/2))
-            x--;
+        while(x &&  (x*(x+1)/2 != n*(n+1)/2 - (x-1)*(x)/2)) x--;
         return x ? x : -1;
     }
 };
