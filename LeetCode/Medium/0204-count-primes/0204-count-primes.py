127860ce2c4e24436a9139a5e5b4a328e93b6d3a
msk_reddy_chirla <mskreddy9963@gmail.com>
2026-08-09 12:15:24 +0530

    Time: 2430 ms (80.82%), Space: 130.7 MB (47.6%) - LeetHub

--- LeetCode/Medium/0204-count-primes/0204-count-primes.py
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.py
@@ -1,15 +1,4 @@
-import math
-
 class Solution(object):
-    def create(self, n):
-        self.box = [True] * n
-        self.box[0],self.box[1] = False,False
-
-        for i in range(2,int(n**0.5+1)):
-            if self.box[i]:
-                for j in range(i*i,n,i):
-                    self.box[j] = False
-
     def countPrimes(self, n):
         """
         :type n: int
@@ -18,7 +7,13 @@ class Solution(object):
         if(n<=1): 
             return 0
 
-        self.create(n)
+        box = [True] * n
+        box[0],box[1] = False,False
 
-        return sum(self.box)
+        for i in range(2,int(n**0.5)+1):
+            if box[i]:
+                for j in range(i*i,n,i):
+                    box[j] = False
+
+        return sum(box)
        
