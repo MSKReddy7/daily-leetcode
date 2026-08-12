4639c308e665b8256f702c0bf02636fa772c293d
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-12 09:40:53 +0530

    Time: 74 ms (38.37%), Space: 111.2 MB (56.42%) - LeetHub

---
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(auto i : nums){
+            if(s.contains(i)) return 1;
+            s.insert(i);
+        }
+        return 0;
+    }
+};
