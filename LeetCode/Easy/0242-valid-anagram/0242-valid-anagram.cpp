dd2bb7f92c803ad9b6c21bd9df2960efa1ee3d11
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-12 09:22:14 +0530

    Time: 7 ms (18.21%), Space: 9.5 MB (92.92%) - LeetHub

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        if(s.length() != t.length()) return 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i]!=t[i]) return 0;
+        }
+
+        return 1;
+
+    }
+};
