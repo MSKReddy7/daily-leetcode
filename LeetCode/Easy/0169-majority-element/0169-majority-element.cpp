ee85bf6a7e885002436b5f5ae18f8983868e7395
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-15 22:59:29 +0530

    Time: 0 ms (100%), Space: 28.3 MB (43.42%) - LeetHub

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto it: nums) m[it]++;
+        for(auto it: m){
+            if(it.second > nums.size()/2) return it.first;
+        }
+        return 0;
+    }
+};
