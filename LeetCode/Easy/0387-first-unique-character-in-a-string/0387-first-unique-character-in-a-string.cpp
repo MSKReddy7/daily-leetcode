552fb52df78d93049f1951accece61bfc560e1ff
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-16 13:31:54 +0530

    Time: 10 ms (51.94%), Space: 14.9 MB (39.24%) - LeetHub

---
+++ LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> m;
+        for(auto i:s) m[i]++;
+
+        for(auto i : s){
+            if(m[i] == 1){
+                return s.find(i);
+                break;
+            }
+        }   
+
+        return -1;
+    }
+};
