94d70228011da17b2caad0d6518b00f14f8d6458
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-19 20:03:20 +0530

    Time: 0 ms (100%), Space: 95.9 MB (18.79%) - LeetHub

--- LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,9 +3,11 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int i=0;
         int j=0;
+        
         int n1 = nums1.size();
         int n2 = nums2.size();
         int n3 = n1+n2;
+        
         vector<int> merged;
 
         while(i<n1 && j<n2){
@@ -18,10 +20,9 @@ public:
         while(i<n1) merged.push_back(nums1[i++]);
         while(j<n2) merged.push_back(nums2[j++]);
 
-        if(n3 % 2){
+        if(n3 % 2)
             return merged[n3/2];
-        }
-        return (merged[n3/2-1]+merged[n3/2])/2.0;
 
+        return (merged[n3/2-1]+merged[n3/2])/2.;
     }
 };
