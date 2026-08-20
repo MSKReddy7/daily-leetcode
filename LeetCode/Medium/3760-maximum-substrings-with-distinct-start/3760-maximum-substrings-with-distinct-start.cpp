1267b0ca934ded43fcc651426ab680d63c19b324
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-20 23:38:10 +0530

    Time: 29 ms (48.27%), Space: 25.3 MB (24.84%) - LeetHub

--- LeetCode/Medium/3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ LeetCode/Medium/3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxDistinct(string s) {
         int count = 0;
-        set<char> st;
+        unordered_set<char> st;
         for(auto i: s) 
             st.insert(i);
         return st.size();
