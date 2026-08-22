d4cde8e6c3443537de8561cf658441d59edc2478
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-22 20:50:45 +0530

    Time: 3 ms (46.32%), Space: 25.2 MB (73.99%) - LeetHub

---
+++ LeetCode/Easy/0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        int prev = -1;
+        int csum = 0;
+        int d;
+        for(auto i: nums) csum+=i;
+        for(int i =0; i<n-1; i++){
+            if(nums[i] ==  nums[i+1]) d = nums[i];
+        }
+
+        cout << d << endl;
+
+        return {d,sum - (csum-d)};
+    }
+};
