652f2c4e7c129186803d79781f5a5f9db84c9135
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-23 23:18:06 +0530

    Time: 0 ms (100%), Space: 12.1 MB (17.75%) - LeetHub

---
+++ LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m;
+        
+        for(auto i: arr) m[i]++;
+
+        unordered_set<int> s;
+        for(auto i: m){
+            if(s.contains(i.second))
+                return false;
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
