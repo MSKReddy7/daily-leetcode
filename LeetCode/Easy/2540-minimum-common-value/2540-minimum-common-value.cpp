2bc9db9543fb62855a9958cfbd088516bc409bdb
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-23 09:37:27 +0530

    Time: 0 ms (100%), Space: 54.4 MB (82.52%) - LeetHub

--- LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.cpp
+++ LeetCode/Easy/2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        for(int i=0; i<nums1.size(); i++){
-            if(binary_search(nums2.begin(),nums2.end(),nums1[i])) return nums1[i];
+        int i=0, j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] == nums2[j]) return nums1[i];
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
         }
         return -1;
     }
