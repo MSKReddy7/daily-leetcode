47435076b68b4e0f746f3b74df2f084314570dfd
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-24 14:29:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (95.38%) - LeetHub

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,10 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int t) {
         int i=0;
         int j=nums.size()-1;
-        while((nums[i]+nums[j]) != t){
+        int sum = nums[i]+nums[j];
+
+        while(sum != t){
             if(nums[i]+nums[j] < t) i++;
             else j--;
-            cout << i << ' ' << j << endl;
+            sum = nums[i]+nums[j];
         }
 
         return {i+1,j+1};
