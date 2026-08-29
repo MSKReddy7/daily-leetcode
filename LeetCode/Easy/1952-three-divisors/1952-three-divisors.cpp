beb34e4ecd3413702b279b7d12744075e1105fef
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-08-29 14:03:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (57.76%) - LeetHub

--- LeetCode/Easy/1952-three-divisors/1952-three-divisors.cpp
+++ LeetCode/Easy/1952-three-divisors/1952-three-divisors.cpp
@@ -2,7 +2,7 @@ int divs(int n){
     int count = 2;
     for(int i=2; i<n; i++){
         if(n%i == 0) count++;
-        if(count >3) break;
+        if(count > 3) break;
     }
     return count;
 }
