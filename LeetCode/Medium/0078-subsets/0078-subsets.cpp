48caa651b0c029b1835e3f09fe05e51d26a06d41
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-02 11:12:38 +0530

    Time: 0 ms (100%), Space: 9.9 MB (82.31%) - LeetHub

--- LeetCode/Medium/0078-subsets/0078-subsets.cpp
+++ LeetCode/Medium/0078-subsets/0078-subsets.cpp
@@ -1,16 +1,22 @@
+void gen(vector<int>& nums,int idx, vector<vector<int>>& res, int n, vector<int>& sub){
+    if(idx == n){
+        res.push_back(sub);
+        return;
+    }
+    sub.push_back(nums[idx]);
+    gen(nums, idx+1, res, n, sub);
+    sub.pop_back();
+    gen(nums, idx+1, res, n, sub);
+}
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = 1<<nums.size();
+        vector<vector<int>> res;
+        vector<int> sub;
+
+        gen(nums, 0, res, nums.size(), sub);
         
-        vector<vector<int>> res(n);
-
-        int take = 0;
-
-        for(int take = 0; take < n; take++) 
-            for(int i=0; i<nums.size(); i++)
-                if((take>>i) & 1)  res[take].push_back(nums[i]);
-    
         return res;
     }
 };
