b515ff49ac448b6e40d3fee5b2ec32a660befe9a
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-12 00:14:56 +0530

    Time: 872 ms (5.23%), Space: 30.9 MB (51.66%) - LeetHub

--- LeetCode/Easy/3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ LeetCode/Easy/3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int totalNumbers(vector<int>& d) {
         int n = d.size();
-        set<int> s;
+        unordered_set<int> s;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(i!=j){
                     for(int k=0; k<n; k++){
                         if(k!=i && k!=j){
                             cout << d[i] << ' ' << d[j] << ' ' << d[k] << endl;
-                            if( d[i] && ((d[k])&1) == 0)
+                            if(d[i] && ((d[k])&1) == 0)
                                 s.insert((d[i]*10 + d[j])*10 + d[k]);
                         }
                     }
