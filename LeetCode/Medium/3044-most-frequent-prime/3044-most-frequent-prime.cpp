0635a6df94667976a852b52eaabcd3baa223aed9
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-16 23:16:40 +0530

    Time: 1133 ms (8.81%), Space: 327.9 MB (5.03%) - LeetHub

--- LeetCode/Medium/3044-most-frequent-prime/3044-most-frequent-prime.cpp
+++ LeetCode/Medium/3044-most-frequent-prime/3044-most-frequent-prime.cpp
@@ -3,21 +3,23 @@ public:
     int mostFrequentPrime(vector<vector<int>>& mat) {
         int r = mat.size();
         int c = mat[0].size();
-        
-        if(max(r,c)<=1) return -1;
-        
-        int n = (int)pow(10,max(r,c));
-        vector<bool> sieve(n+1,true);
+
+        int m = max(r,c);
+        if(m <= 1) return -1;
+
+        int n = 1;
+        for(int i=0; i<m; i++) n*=10;
+
+        vector<char> sieve(n, true);
         sieve[0] = sieve[1] = false;
 
-        for(int i=2; i*i< n; i++)
+        for(int i=2; i*i<n; i++)
             if(sieve[i])
-                for(int j=i*i; j<=n; j+=i)
+                for(int j=i*i; j<n; j+=i)
                     sieve[j] = false;
-            
-        
-        map<int,int> primefreq;
-        
+
+        vector<int> primefreq(n);
+
         int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
         int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
 
@@ -29,21 +31,26 @@ public:
                     int y = j;
                     while(x>=0 && y>=0 && x<r && y<c){
                         num = num*10 + mat[x][y];
-                        if(num>10 && sieve[num]) primefreq[num]++;
-                        x += dx[k];
-                        y += dy[k];
+                        if(num >= n) break;
+                        if(num > 10 && sieve[num])
+                            primefreq[num]++;
+                        x+=dx[k];
+                        y+=dy[k];
                     }
                 }
             }
         }
-        int mx = -1;
+
+        int mx = 0;
         int ans = -1;
-        for(auto it: primefreq){
-            if(it.second >= mx) {
-                ans = it.first;
-                mx = it.second;
+
+        for(int i = 2; i < n; i++) {
+            if(primefreq[i] > 0 && primefreq[i] >= mx && sieve[i]) {
+                mx = primefreq[i];
+                ans = i;
             }
         }
+
         return ans;
     }
 };
