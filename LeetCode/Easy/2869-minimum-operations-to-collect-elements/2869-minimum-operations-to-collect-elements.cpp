ab36991e686b66b208e59ead24eba49760e795f6
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-18 20:28:25 +0530

    Time: 24 ms (2.06%), Space: 22.8 MB (47.94%) - LeetHub

---
+++ LeetCode/Easy/2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        int sum = k*(k+1)/2;
+        unordered_set<int> seen;
+        int i=n-1;
+        while(i>=0 && sum){
+            if(nums[i] <= k){
+                if(seen.contains(nums[i])){
+                    i--;
+                    continue;
+                } 
+                sum-=nums[i];
+                seen.insert(nums[i]);
+            } 
+            cout << i << ' ' << nums[i] << ' ' << endl;
+            i--;
+        }
+        return n-(i+1);
+    }
+};
