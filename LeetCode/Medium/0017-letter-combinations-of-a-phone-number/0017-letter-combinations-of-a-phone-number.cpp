acfb4cc28f182f8850979d376f87ffea2354bf45
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-18 14:44:48 +0530

    Time: 0 ms (100%), Space: 8.9 MB (96.46%) - LeetHub

---
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,30 @@
+void generate(string& digits, int idx, unordered_map<int,string>& mp, vector<string>& res,string& temp){
+    if(idx == digits.length()){
+        res.push_back(temp);
+        return;
+    }
+    for(auto i: mp[digits[idx] - '0']){
+        cout << i << ' ';
+        temp.push_back(i);
+        generate(digits,idx+1,mp,res,temp);
+        temp.pop_back();
+    }
+}
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        unordered_map<int,string> mp = {
+                       {2,"abc"}, {3,"def"},
+            {4,"ghi"}, {5,"jkl"}, {6,"mno"},
+            {7,"pqrs"},{8,"tuv"}, {9,"wxyz"}
+        };
+
+        vector<string> res;
+        string temp = "";
+
+        generate(digits,0,mp,res,temp);
+        return res;
+    }
+
+};
