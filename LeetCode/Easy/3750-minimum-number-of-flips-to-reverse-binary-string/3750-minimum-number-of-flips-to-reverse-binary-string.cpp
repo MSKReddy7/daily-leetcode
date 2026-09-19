e24ebf0255485e1fea16f7b2b8ea8d6819f14017
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-20 01:25:23 +0530

    Time: 0 ms (100%), Space: 8.7 MB (81.99%) - LeetHub

---
+++ LeetCode/Easy/3750-minimum-number-of-flips-to-reverse-binary-string/3750-minimum-number-of-flips-to-reverse-binary-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumFlips(int n) {
+        int count = 0;
+        int i = 0;
+        int j = log2(n);
+
+        while(i<j){
+            int lb = n>>i & 1;
+            int rb = n>>j & 1;
+            if(lb!=rb) count++;
+            i++;j--;
+        }
+        return count*2;
+    }
+};
