51927e9059d1fa5cded56ac0cdcd3f7ef9482f26
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-19 19:09:04 +0530

    Time: 0 ms (100%), Space: 7.7 MB (53.42%) - LeetHub

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num == 1) return true;
+
+        int l = 1;
+        int r = num/2;
+
+        for(; l<=r; ){
+            long long mid = l+(r-l)/2;
+            if(mid*mid == num) return true;
+            else if(mid*mid > num) r = mid-1;
+            else l = mid+1;
+        }
+        
+        return false;
+    }
+};
