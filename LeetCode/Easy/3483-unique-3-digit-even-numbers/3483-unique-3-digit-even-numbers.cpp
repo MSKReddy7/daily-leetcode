a7118e20fdad05e57272d8dc7cee703c9ebac2ee
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-25 23:04:12 +0530

    Time: 63 ms (11.61%), Space: 40.5 MB (10.05%) - LeetHub

--- LeetCode/Easy/3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ LeetCode/Easy/3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,16 +1,16 @@
-void gen(vector<int>& nums, int n, unordered_set<int>& picked, set<vector<int>>& res, vector<int>& temp){
+void gen(vector<int>& nums, unordered_set<int>& picked, set<vector<int>>& res, vector<int>& temp){
     if(temp.size() == 3){
         if(temp[2]%2) return;
         res.insert(temp);
         return; 
     }
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<nums.size(); i++){
         if(picked.contains(i) || (!temp.size() && !nums[i])) 
             continue;
         picked.insert(i);
         temp.push_back(nums[i]);
-        gen(nums, n, picked, res, temp);
+        gen(nums, picked, res, temp);
         picked.erase(i);
         temp.pop_back();
     }
@@ -23,7 +23,7 @@ public:
         unordered_set<int> picked(3);
         set<vector<int>> res;
         vector<int> temp;
-        gen(nums, n, picked, res, temp);
+        gen(nums, picked, res, temp);
         return res.size();
     }
 };
