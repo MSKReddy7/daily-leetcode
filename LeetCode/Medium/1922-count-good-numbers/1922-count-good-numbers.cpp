ba8cc5c02c08b6dc0e5f0660e75b0974fdaafbf9
Chirla Manoj Sai Kumar Reddy <mskreddy9963@gmail.com>
2026-09-25 12:29:53 +0530

    Time: 0 ms (100%), Space: 8 MB (37.16%) - LeetHub

---
+++ LeetCode/Medium/1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -0,0 +1,15 @@
+typedef long long ll;
+int mod = 1e9+7;
+
+long long getPow(ll a, ll b){
+    if(!b) return 1;
+    ll res = getPow(a,b/2)%mod;
+    return (b%2 ? a : 1) * res*res%mod;
+}
+
+class Solution {
+public:
+    int countGoodNumbers(long long n) {
+        return getPow(5,(n+1)/2)%mod * getPow(4,n/2)%mod % mod;
+    }
+};
